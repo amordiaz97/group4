7b2bd80a28a9e1fd7bb70875f07a92c764868b4a
jcolladokuri <cssi2016@CSSIs-MacBook-Air-2.local>
2016-08-03 11:49:32 -0500

    no comments

--- project4/main.py
+++ project4/main.py
@@ -61,8 +61,7 @@ class foodHandler(webapp2.RequestHandler):
 		template = env.get_template('food.html')
 		self.response.write(template.render())
 
-class MatchingSongHandler(webapp2.RequestHandler):  
-<<<<<<< HEAD
+class MatchingSongHandler(webapp2.RequestHandler):
 	def post(self):
 			template = env.get_template('song.html')
 			songs = {"Crazy in Love - Beyonce" : "https://www.youtube.com/embed/ViwtNLUqkMY" , 
@@ -339,45 +338,11 @@ class MatchingSongHandler(webapp2.RequestHandler):
 				for key, value in songs.iteritems():
 					if value == "https://www.youtube.com/embed/FyASdjZE0R0":
 							result = key
-				data = {"link": val, 'result': result}		
-
-	
-#rick	
-																						# 
+				data = {"link": val, 'result': result}										#
 			self.response.write(template.render(data))
 
 
-=======
-	def get(self):
-		songs = {"Crazy in Love - Beyonce" : "https://www.youtube.com/embed/ViwtNLUqkMY" , 
-		"This Is What You Came For - Calvin Harris ft. Rihanna" : "https://www.youtube.com/embed/kOkQ4T5WO9E" , 
-		"Dani California - Red Hot Chili Peppers" : "https://www.youtube.com/embed/Sb5aq5HcS1A" , 
-		"Cosmic Love - Florence and the Machine" : "https://www.youtube.com/embed/2EIeUlvHAiM", 
-		"Hips Don't Lie - Shakira" : "https://www.youtube.com/embed/DUT5rEU6pqM",
-		"Stairway to Heaven - Led Zeppelin" : "https://www.youtube.com/embed/oW_7XBrDBAA",
-		"Work It - Missy Elliot" : "https://www.youtube.com/embed/cjIvu7e6Wq8",
-		"Unsteady - X Ambassadors" : "https://www.youtube.com/embed/V0lw3qylVfY",
-		"Promise - Romeo Santos ft Usher" : "https://www.youtube.com/embed/Y3XyWhrZnqE",
-		"No Problem - Chance The Rapper ft. Lil Wayne & 2 Chainz" : "https://www.youtube.com/embed/_2LXpNmjxMw",
-		"Homecoming - Kanye ft. Chris Martin" : "https://www.youtube.com/embed/LQ488QrqGE4",
-		"Viva La Vida - Coldplay" : "https://www.youtube.com/embed/Tmb7YIKqLeM",
-		"Where Ya At - Drake & Future" : "https://www.youtube.com/embed/lw3Or6eqIpI",
-		"Friday - Rebecca " : "https://www.youtube.com/embed/kfVsfOSbJY0",
-		"Single Ladies - Beyonce" : "https://www.youtube.com/embed/4m1EFMoRFvY",
-		"Dancing Queen - ABBA" : "https://www.youtube.com/embed/y62OlGvC-bk",
-		"Dream On - Aerosmith" : "https://www.youtube.com/embed/hHRNSeuvzlM",
-		"Bohemian Rhapsody - Queen" : "https://www.youtube.com/embed/fJ9rUzIMcZQ",
-		"Never Gonna Give You Up - Rick Astley" : "https://www.youtube.com/embed/dQw4w9WgXcQ",
-		"Roses - Chainsmokers ft. Rozes" : "https://www.youtube.com/embed/FyASdjZE0R0"
-		}
-		
-		template = env.get_template('song.html')
-		result = random.choice(songs.keys())
-		value = songs[result]
-		data = {'link' : value, 'result': result}
-		self.response.write(template.render(data)) #we need to add the dictionary that will pass on the variable(song) to the document
-# class MatchingFoodHandler(webapp2.Request)
->>>>>>> origin/master
+	
 
 class MatchingFoodHandler(webapp2.RequestHandler):
 	def get(self):
